6acddd6e084a3775c364d4e168d183e8a638472f
mcheng3 <michael.cheng65@gmail.com>
2017-12-10 14:34:04 -0500

    Finish intro.js

--- intro.js
+++ intro.js
@@ -1,19 +1,45 @@
+var x = 8
+
 var addElement = function(){
 	var list = document.getElementById('thelist');
 	var newElement = document.createElement('li');
-	newElement.innerHTML = 'item 10';
+	newElement.innerHTML = 'item ' + x;
+	x++;
 	list.appendChild(newElement);
+	newElement.addEventListener('mouseover', changeHeading);
+	newElement.addEventListener('mouseout', removeHeading);
+	newElement.addEventListener('click', removeItem);
 	
 };
 
-var changeHeading = function(h){
+var changeHeading = function(){
+	//console.log(this);
 	var heading = document.getElementById('h');
-	heading.innerHTML = h;
-}
+	heading.innerHTML = this.innerHTML;
+};
+
+var removeHeading = function(){
+	//console.log(this);
+	var heading = document.getElementById('h');
+	heading.innerHTML = 'Hello World!';
+};
+
+
+var removeItem = function(){
+	this.remove();
+};
 	
+var listItems = document.getElementsByTagName('li');
+var i;
+for(i = 0; i < listItems.length; i++){
+	listItems[i].addEventListener('mouseover', changeHeading);
+	listItems[i].addEventListener('mouseout', removeHeading);
+	listItems[i].addEventListener('click', removeItem);
+}
 
 var b = document.getElementById('b');
 b.addEventListener( 'click', addElement);
 
-var listItems = document.getElementsByTagName('li');
-for (i = 0; i < listItems.length; 
+
+
+
