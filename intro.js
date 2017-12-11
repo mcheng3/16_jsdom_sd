70a43a2c38403593274e09af2f86b96b59d73c73
Jake Goldman <jgoldman@stuy.edu>
2017-12-11 14:33:08 -0500

    finished classwork from 12/11

--- intro.js
+++ intro.js
@@ -1,6 +1,10 @@
+
+
+
+// -------------------------PART 1-----------------------------------
 var x = 8
 
-var addElement = function(){
+var addElement = function(e){
 	var list = document.getElementById('thelist');
 	var newElement = document.createElement('li');
 	newElement.innerHTML = 'item ' + x;
@@ -9,27 +13,28 @@ var addElement = function(){
 	newElement.addEventListener('mouseover', changeHeading);
 	newElement.addEventListener('mouseout', removeHeading);
 	newElement.addEventListener('click', removeItem);
-	
+
 };
 
-var changeHeading = function(){
-	//console.log(this);
+var changeHeading = function(e){
+	// console.log(this);
 	var heading = document.getElementById('h');
 	heading.innerHTML = this.innerHTML;
 };
 
-var removeHeading = function(){
-	//console.log(this);
+var removeHeading = function(e){
+	// console.log(this);
 	var heading = document.getElementById('h');
 	heading.innerHTML = 'Hello World!';
 };
 
 
-var removeItem = function(){
+var removeItem = function(e){
 	this.remove();
 };
-	
-var listItems = document.getElementsByTagName('li');
+
+// loop through the list and add events for each item
+var listItems = document.getElementsByClassName('orig_list');
 var i;
 for(i = 0; i < listItems.length; i++){
 	listItems[i].addEventListener('mouseover', changeHeading);
@@ -41,5 +46,25 @@ var b = document.getElementById('b');
 b.addEventListener( 'click', addElement);
 
 
+// -------------------------PART 2-----------------------------------
+var fibList = document.getElementById('fibList');
+var fibnum = 1;
 
+var fibonacci = function(n){
+    if(n<=2){
+        return 1;
+    }
+    else{
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+}
 
+var addFib = function(e){
+	var newElement = document.createElement('li');
+	newElement.innerHTML = fibonacci(fibnum);
+	fibList.appendChild(newElement);
+	fibnum++;
+}
+
+var fibButton = document.getElementById('fibButton');
+fibButton.addEventListener( 'click', addFib);
